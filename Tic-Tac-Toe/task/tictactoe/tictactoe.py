792343384cb86193b2d1ecf4ca97d8af81449880
Grigory Rodimov <grishanyar@gmail.com>
2020-07-13 21:45:44 +0300

    Tic-Tac-Toe stage #2

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,3 +1,23 @@
-print('''X O X
-O X O
-X X O''')
+user_list = []
+
+while True:
+    user_input = input('Enter cells: ')
+    if len(user_input) == 9:
+        for el in user_input:
+            if el == 'O' or el == 'X' or el == '_':
+                user_list.append(el)
+                continue
+            else:
+                print('Value error')
+                break
+        break
+    else:
+        print('Value error')
+        break
+
+print('---------')
+print('| ' + ' '.join(user_list[:3]), end=' |\n')
+print('| ' + ' '.join(user_list[3:6]), end=' |\n')
+print('| ' + ' '.join(user_list[6:]), end=' |\n')
+print('---------')
+
