906e569fce4c5c880948e220f5580164c6d699f3
Grigory Rodimov <grishanyar@gmail.com>
2020-07-26 18:38:16 +0300

    Word list

---
+++ Problems/Word list/task.py
@@ -0,0 +1,8 @@
+text = [["Glitch", "is", "a", "minor", "problem", "that", "causes", "a", "temporary", "setback"],
+        ["Ephemeral", "lasts", "one", "day", "only"],
+        ["Accolade", "is", "an", "expression", "of", "praise"]]
+
+user_input = int(input())
+
+
+print([j for i in text for j in i if len(j) <= user_input])
